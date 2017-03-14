12db7a78bb7c9700a04d105c420a9c0bebc8a238
JamesChill <rehqzi@outlook.com>
2017-03-14 05:42:50 -0400

    Update james.py

--- james/james.py
+++ james/james.py
@@ -9,7 +9,7 @@ class James:
     async def james(self, ctx):
         author = ctx.message.author
         e = discord.Embed(description="""**Links:**
-Twitch: https://www.twitch.tv/jameschil_l
+Twitch: https://www.twitch.tv/JamesChil_l
 Twitter: https://twitter.com/JamesChil_l
 Discord: https://discord.gg/HU5XzjW
 
