606495d1107e0d9b2f9122cd37f479535c62a13a
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-03-14 04:34:20 -0500

    Create james.py

---
+++ james/james.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class James:
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(pass_context=True):
+    async def james(self, ctx):
+        author = ctx.message.author
+        e = discord.Embed(description="""**Links:**
+Twitch: https://www.twitch.tv/jameschil_l
+Twitter: https://twitter.com/JamesChil_l
+Discord: https://discord.gg/HU5XzjW
+
+**Clips:**
+https://clips.twitch.tv/SavageCreativeElephantBuddhaBar""", colour=author.colour)
+        e.set_field(name=author.name, icon_url=author.avatar_url)
+        await bot.say(embed=e)
+        
+def setup(bot):
+    n = James(bot)
+    bot.add_cog(n)
